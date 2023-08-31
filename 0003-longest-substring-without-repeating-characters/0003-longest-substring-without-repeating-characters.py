cca1deeebf3f32095463f4510b09567931ab9610
Pratham Manja <pratham0762002@gmail.com>
2023-08-31 17:28:32 +0530

    Time: 54 ms (91.14%), Space: 16.2 MB (97.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,11 +5,13 @@ class Solution:
         i=0
         for j in range(len(s)):
             if s[j] not in char:
-                char.add(s[j])#unique character added
-                length=max(length,j-i+1) #update the length after adding the char
+                char.add(s[j])
+                length=max(length,j-i+1)
             else:
-                while s[j] in char:#removes all the old characters(sliding window)
+                while s[j] in char:
                     char.remove(s[i])
                     i+=1
                 char.add(s[j])
-        return length
+        return length       
+
+
