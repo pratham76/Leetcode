354ac566423e15e0e3356b10bde886d40ecaeb30
Pratham Manja <pratham0762002@gmail.com>
2023-10-02 18:48:24 +0530

    Time: 600 ms (53.52%), Space: 29.2 MB (71.46%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -10,6 +10,7 @@ class Solution:
         return res
         """
         #O(N)
+        '''
         l,r=0,len(height)-1
         max_area=0
         while l<r:
@@ -20,6 +21,17 @@ class Solution:
             else:
                 l+=1
         
+        return max_area
+        '''
+        l,r=0,len(height)-1
+        max_area=0
+        while l<r:
+            area=(r-l)*min(height[l],height[r])
+            max_area=max(area,max_area)
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
         return max_area
 
         
