c292edb0a126ac482cc66cef854f7f4eb13eae76
Pratham Manja <pratham0762002@gmail.com>
2023-10-12 18:46:14 +0530

    Time: 560 ms (59.35%), Space: 41.1 MB (92.98%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,6 +6,7 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         # o(n) tine and o(n) space
+        """
         temp=head
         slist=[]
         while temp:
@@ -15,3 +16,27 @@ class Solution:
         if slist==slist[::-1]:
             return True
         return False
+        """
+        #o(1)
+        slow,fast=head,head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        
+        #reverse sencond half of the list
+        prev=None
+        while slow:
+            temp=slow.next
+            slow.next=prev
+            prev=slow
+            slow=temp
+        
+        #check for palindrome
+        left,right=head,prev
+        while right:
+            if left.val!=right.val:
+                return False
+            left=left.next
+            right=right.next
+        return True
+     
