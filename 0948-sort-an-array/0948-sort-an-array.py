c595684ef4ab0ff4a63b579d8365841d4b3b7ea0
Pratham Manja <pratham0762002@gmail.com>
2023-10-13 11:04:42 +0530

    Time: 1452 ms (69.26%), Space: 25.3 MB (36.7%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,31 +1,28 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-            if len(nums) <= 1:
-                return nums
+            if len(nums)<=1:
+               return nums
+            mid=len(nums)//2
+            l,r=nums[:mid],nums[mid:]
+            left,right=self.sortArray(l),self.sortArray(r)
+            return self.merge(left,right)
+    def merge(self,left,right):
+        res=[]
+        i,j=0,0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                res.append(left[i])
+                i+=1
+            else:
+                res.append(right[j])
+                j+=1
+        if i<len(left):
+            res.extend(left[i:])
+        if j<len(right):
+            res.extend(right[j:])
+        
+        return res
 
-            mid = len(nums) // 2
-            left = nums[:mid]
-            right = nums[mid:]
-
-            left = self.sortArray(left)
-            right = self.sortArray(right)
-
-            return self.merge(left, right)
-    def merge(self,list1,list2):
-            res=[]
-            i,j=0,0
-            while i<len(list1) and j<len(list2):
-                if list1[i]<list2[j]:
-                    res.append(list1[i])
-                    i+=1
-                else:
-                    res.append(list2[j])
-                    j+=1
-            if list1:
-                res.extend(list1[i:])
-            if list2:
-                res.extend(list2[j:])
-            return res
         
        
 
