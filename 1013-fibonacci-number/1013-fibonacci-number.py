1c69676ede027266753d0cb5e50fa914199b179a
Pratham Manja <pratham0762002@gmail.com>
2023-10-21 12:27:36 +0530

    Time: 600 ms (26.56%), Space: 13.4 MB (12.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,8 +1,13 @@
-class Solution:
-    def fib(self, n: int) -> int:
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         if n==0:
             return 0
         if n==1:
             return 1
         else:
             return self.fib(n-1)+self.fib(n-2)
+        
