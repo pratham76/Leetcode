1faac936a50567f808f7dff059ffc32f49dc89a5
Pratham Manja <pratham0762002@gmail.com>
2023-10-21 12:19:42 +0530

    Time: 18 ms (17.65%), Space: 13.1 MB (69.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,12 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         one,two=1,1
         for i in range(n-1):
             temp=one
             one=one+two
             two=temp
         return one
-        
-        
