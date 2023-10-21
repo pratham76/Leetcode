3f7a3021dd3df844b46033cf8fc0f018f3facc51
Pratham Manja <pratham0762002@gmail.com>
2023-10-21 12:40:54 +0530

    Time: 2045 ms (57.5%), Space: 13.5 MB (91.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,11 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums: return 
-        res=[1]*len(nums)
+        if not nums: return []
+        dp=[1]*len(nums)
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i]>nums[j]:
-                    res[i]=max(res[i],res[j]+1)
-        return max(res)
-        
+                    dp[i]=max(dp[i],dp[j]+1)
+        return max(dp)
