824fe5649f048abb6f421c025b66485ee44ff4f8
Pratham Manja <pratham0762002@gmail.com>
2023-10-25 16:07:18 +0530

    Time: 463 ms (93.25%), Space: 34.6 MB (67.41%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type text1: str
         :type text2: str
         :rtype: int
-        """
+        
         t1=len(text1)
         t2=len(text2)
         dp=[[0]*(t2+1) for _ in range(t1+1)]
@@ -16,5 +16,16 @@ class Solution(object):
                 else:
                     dp[i][j]=max(dp[i][j+1],dp[i+1][j])
         return dp[0][0]
-            
+        """
+
+        t1,t2=len(text1),len(text2)
+        dp=[[0]*(t2+1) for _ in range(t1+1)]
+
+        for i in range(t1-1,-1,-1):
+            for j in range(t2-1,-1,-1):
+                if text1[i]==text2[j]:
+                    dp[i][j]=1+dp[i+1][j+1]
+                else:
+                    dp[i][j]=max(dp[i][j+1],dp[i+1][j])
+        return dp[0][0]
         
