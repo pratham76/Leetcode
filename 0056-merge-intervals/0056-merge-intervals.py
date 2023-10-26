aa17ab20b30f9ab78bb7a53f1025d5620c0893ec
Pratham Manja <pratham0762002@gmail.com>
2023-10-26 12:52:02 +0530

    Time: 107 ms (28.85%), Space: 19.4 MB (13.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,24 +1,16 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        #O(nlogn)
-        '''
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
         intervals.sort(key=lambda i:i[0])
         output=[intervals[0]]
+
         for start,end in intervals[1:]:
             lastend=output[-1][1]
             if start<=lastend:
                 output[-1][1]=max(lastend,end)
             else:
                 output.append([start,end])
-        return output
-        '''
-
-        intervals.sort(key=lambda i:i[0])
-        output=[intervals[0]]
-        for i,j in intervals[1:]:
-            le=output[-1][1]
-            if i<=le:
-                output[-1][1]=max(le,j)
-            else:
-                output.append([i,j])
         return output
