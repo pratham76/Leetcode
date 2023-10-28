d824a52e04ac17f4699ab097e89a5977bde9f1f8
Pratham Manja <pratham0762002@gmail.com>
2023-10-29 02:23:46 +0530

    Time: 171 ms (38.31%), Space: 20.6 MB (76.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,8 +6,7 @@ class Solution(object):
         """
         
         res=[1]*len(nums)
-        pre=1
-        pos=1
+        pre,pos=1,1
         for i in range(len(nums)):
             res[i]=pre
             pre*=nums[i]
