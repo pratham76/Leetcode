9c9c41572a53195bed24bdd2290d236bc9941307
Pratham Manja <pratham0762002@gmail.com>
2023-10-28 11:02:20 +0530

    Time: 91 ms (94.51%), Space: 19.3 MB (24.91%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,7 +3,20 @@ class Solution(object):
         """
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
+      
+
+        intervals.sort(key=lambda i:i[0])
+        output=[intervals[0]]
+
+        for start,end in intervals[1:]:
+            lastend=output[-1][1]
+            if start<=lastend:
+                output[-1][1]=max(lastend,end)
+            else:
+                output.append([start,end])
+        return output
         """
+
         intervals.sort(key=lambda i:i[0])
         output=[intervals[0]]
 
