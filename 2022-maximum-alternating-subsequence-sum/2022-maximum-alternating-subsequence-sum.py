6fd51dc861d5d2e6b7dd034ccbb81c25e187799c
Pratham Manja <pratham0762002@gmail.com>
2023-10-31 11:17:08 +0530

    Time: 821 ms (78.79%), Space: 25.2 MB (63.64%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
@@ -1,23 +1,13 @@
-class Solution:
-    def maxAlternatingSum(self, nums: List[int]) -> int:
+class Solution(object):
+    def maxAlternatingSum(self, nums):
         """
-        # recursive o(n) and o(n)
-
-        dp={}
-        def dfs(i,even):
-            if i==len(nums):
-                return 0
-            if (i,even) in dp:
-                return dp[(i,even)]
-            total=nums[i] if even else (-1*nums[i])
-            dp[(i,even)]=max(total+dfs(i+1,not even),dfs(i+1,even))
-            return dp[(i,even)]
-        return dfs(0,True)
+        :type nums: List[int]
+        :rtype: int
         """
-        #0(n) and o(1)
         seven,sodd=0,0
         for i in range(len(nums)-1,-1,-1):
-            tmpeven=max(sodd + nums[i],seven)
-            tmpodd=max(seven - nums[i],sodd)
+            tmpeven=max(sodd+nums[i],seven)
+            tmpodd=max(seven-nums[i],sodd)
             seven,sodd=tmpeven,tmpodd
-        return seven
+        
+        return max(seven,sodd)
