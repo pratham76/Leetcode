6c69ea5e8a8d0a77071bf8224f17cba481bc6d80
Pratham Manja <pratham0762002@gmail.com>
2023-11-01 19:19:57 +0530

    Time: 493 ms (88.82%), Space: 34.7 MB (59.05%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,9 +1,12 @@
-class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
         
         t1=len(text1)
         t2=len(text2)
-
         dp=[[0]*(t2+1) for _ in range(t1+1)]
 
         for i in range(t1-1,-1,-1):
@@ -11,6 +14,18 @@ class Solution:
                 if text1[i]==text2[j]:
                     dp[i][j]=1+dp[i+1][j+1]
                 else:
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1])
+                    dp[i][j]=max(dp[i][j+1],dp[i+1][j])
         return dp[0][0]
-        
+        """
+
+        t1=len(text1)
+        t2=len(text2)
+        dp=[[0]*(t2+1) for i in range(t1+1)]
+
+        for i in range(t1-1,-1,-1):
+            for j in range(t2-1,-1,-1):
+                if text1[i]==text2[j]:
+                    dp[i][j]=1+dp[i+1][j+1]
+                else:
+                    dp[i][j]=max(dp[i][j+1],dp[i+1][j])
+        return dp[0][0]
