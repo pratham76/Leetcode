913365dc1741e0f9549a6426f24922ba09db4344
Pratham Manja <pratham0762002@gmail.com>
2023-11-03 19:33:08 +0530

    Time: 407 ms (57.13%), Space: 13.3 MB (89.08%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,44 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        
+        def ispal(a):
+            return a==a[::-1]
+                
+
+        longest=""
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                substr=s[i:j]
+                if ispal(substr) and len(substr)>len(longest):
+                    longest=substr
+        return longest
+        """
         res=""
-        resLen=0
+        longpal=0
 
         for i in range(len(s)):
-            #oddlength
+            #oddlen
             l,r=i,i
             while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1)>resLen:
+                if (r-l+1)>longpal:
                     res=s[l:r+1]
-                    resLen=r-l+1
-                l-=1
+                    longpal=r-l+1
                 r+=1
-            
-            #even length
+                l-=1
+            #evenlen
             l,r=i,i+1
             while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1)>resLen:
+                if (r-l+1)>longpal:
                     res=s[l:r+1]
-                    resLen=r-l+1
-                l-=1
+                    longpal=r-l+1
                 r+=1
+                l-=1
         
         return res
-
-
-
-
-
-
-
-
+            
+                    
 
         
